476dea8737f3512cc45619d1083266f398f7ebc0
OlehMarchenko <oleh.marchenko.95@gmail.com>
2020-03-16 14:34:45 +0200

    fixed tests

--- test/dart_package_template_test.dart
+++ test/dart_package_template_test.dart
@@ -2,7 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:test/test.dart';
+
 /// to run tests use `pub run test`
 void main() {
-  // TODO: Add tests here
+  test('test', () {
+    // TODO: Add tests here
+  });
 }
